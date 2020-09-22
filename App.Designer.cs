30b1315fa081fe9175d5a30dec001cc635e1cd31
Daniele Viana Dos Santos <daniele.santos@hbsis.com.br>
2020-09-22 19:37:03 -0300

    Adicionar arquivos de projeto.

---
+++ App.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace WardrobApp
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnaddform = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnshowallform = new System.Windows.Forms.Button();
+            this.btnseekform = new System.Windows.Forms.Button();
+            this.txtclothestyle = new System.Windows.Forms.TextBox();
+            this.txtobsform = new System.Windows.Forms.TextBox();
+            this.txtlistofclothesform = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.typesoffabric = new System.Windows.Forms.ComboBox();
+            this.clothestype = new System.Windows.Forms.ComboBox();
+            this.colorsset = new System.Windows.Forms.ComboBox();
+            this.txtday = new System.Windows.Forms.TextBox();
+            this.txtmonth = new System.Windows.Forms.TextBox();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.Iddaprocurada = new System.Windows.Forms.Label();
+            this.txtiddaprocuradaform = new System.Windows.Forms.TextBox();
+            this.btnalteraform = new System.Windows.Forms.Button();
+            this.btndelroupaform = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(45, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tipo da Roupa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(45, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estilo da Roupa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(28, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tecido da Roupa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(45, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Cor da Roupa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(45, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Observação";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(45, 375);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 23);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Data de Uso";
+            // 
+            // btnaddform
+            // 
+            this.btnaddform.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnaddform.Location = new System.Drawing.Point(12, 507);
+            this.btnaddform.Name = "btnaddform";
+            this.btnaddform.Size = new System.Drawing.Size(119, 47);
+            this.btnaddform.TabIndex = 1;
+            this.btnaddform.Text = "CADASTRAR";
+            this.btnaddform.UseVisualStyleBackColor = true;
+            this.btnaddform.Click += new System.EventHandler(this.btnaddform_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-9, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(621, 588);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnshowallform
+            // 
+            this.btnshowallform.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnshowallform.Location = new System.Drawing.Point(149, 509);
+            this.btnshowallform.Name = "btnshowallform";
+            this.btnshowallform.Size = new System.Drawing.Size(89, 47);
+            this.btnshowallform.TabIndex = 1;
+            this.btnshowallform.Text = "LISTAR";
+            this.btnshowallform.UseVisualStyleBackColor = true;
+            this.btnshowallform.Click += new System.EventHandler(this.btnshowallform_Click);
+            // 
+            // btnseekform
+            // 
+            this.btnseekform.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnseekform.Location = new System.Drawing.Point(254, 509);
+            this.btnseekform.Name = "btnseekform";
+            this.btnseekform.Size = new System.Drawing.Size(104, 47);
+            this.btnseekform.TabIndex = 1;
+            this.btnseekform.Text = "BUSCAR";
+            this.btnseekform.UseVisualStyleBackColor = true;
+            this.btnseekform.Click += new System.EventHandler(this.btnseekform_Click);
+            // 
+            // txtclothestyle
+            // 
+            this.txtclothestyle.Location = new System.Drawing.Point(12, 84);
+            this.txtclothestyle.Name = "txtclothestyle";
+            this.txtclothestyle.Size = new System.Drawing.Size(175, 23);
+            this.txtclothestyle.TabIndex = 2;
+            // 
+            // txtobsform
+            // 
+            this.txtobsform.Location = new System.Drawing.Point(12, 235);
+            this.txtobsform.Multiline = true;
+            this.txtobsform.Name = "txtobsform";
+            this.txtobsform.Size = new System.Drawing.Size(175, 141);
+            this.txtobsform.TabIndex = 2;
+            // 
+            // txtlistofclothesform
+            // 
+            this.txtlistofclothesform.Location = new System.Drawing.Point(209, 35);
+            this.txtlistofclothesform.Multiline = true;
+            this.txtlistofclothesform.Name = "txtlistofclothesform";
+            this.txtlistofclothesform.Size = new System.Drawing.Size(390, 468);
+            this.txtlistofclothesform.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(301, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 23);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Lista de Roupas";
+            // 
+            // typesoffabric
+            // 
+            this.typesoffabric.FormattingEnabled = true;
+            this.typesoffabric.Items.AddRange(new object[] {
+            "Jeans",
+            "Não jeans"});
+            this.typesoffabric.Location = new System.Drawing.Point(12, 134);
+            this.typesoffabric.Name = "typesoffabric";
+            this.typesoffabric.Size = new System.Drawing.Size(175, 23);
+            this.typesoffabric.TabIndex = 3;
+                       // 
+            // clothestype
+            // 
+            this.clothestype.FormattingEnabled = true;
+            this.clothestype.Items.AddRange(new object[] {
+            "Blusa",
+            "Body",
+            "Calça",
+            "Cardigã/Casaco/Jaqueta",
+            "Jardineira",
+            "Lingerie",
+            "Macacão",
+            "Macaquinho",
+            "Pijama",
+            "Saia",
+            "Short",
+            "Vestido"});
+            this.clothestype.Location = new System.Drawing.Point(12, 35);
+            this.clothestype.Name = "clothestype";
+            this.clothestype.Size = new System.Drawing.Size(175, 23);
+            this.clothestype.TabIndex = 3;
+            // 
+            // colorsset
+            // 
+            this.colorsset.FormattingEnabled = true;
+            this.colorsset.Items.AddRange(new object[] {
+            "Amarelo",
+            "Azul",
+            "Bege",
+            "Branco",
+            "Cinza",
+            "Laranja",
+            "Marrom",
+            "Preto",
+            "Rosa",
+            "Roxo",
+            "Verde",
+            "Vermelho",
+            "Vinho"});
+            this.colorsset.Location = new System.Drawing.Point(12, 186);
+            this.colorsset.Name = "colorsset";
+            this.colorsset.Size = new System.Drawing.Size(175, 23);
+            this.colorsset.TabIndex = 3;
+            // 
+            // txtday
+            // 
+            this.txtday.Location = new System.Drawing.Point(12, 401);
+            this.txtday.Name = "txtday";
+            this.txtday.Size = new System.Drawing.Size(39, 23);
+            this.txtday.TabIndex = 2;
+            // 
+            // txtmonth
+            // 
+            this.txtmonth.Location = new System.Drawing.Point(68, 401);
+            this.txtmonth.Name = "txtmonth";
+            this.txtmonth.Size = new System.Drawing.Size(39, 23);
+            this.txtmonth.TabIndex = 2;
+            // 
+            // txtyear
+            // 
+            this.txtyear.Location = new System.Drawing.Point(118, 401);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(69, 23);
+            this.txtyear.TabIndex = 2;
+            // 
+            // Iddaprocurada
+            // 
+            this.Iddaprocurada.AutoSize = true;
+            this.Iddaprocurada.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Iddaprocurada.Location = new System.Drawing.Point(12, 454);
+            this.Iddaprocurada.Name = "Iddaprocurada";
+            this.Iddaprocurada.Size = new System.Drawing.Size(187, 23);
+            this.Iddaprocurada.TabIndex = 1;
+            this.Iddaprocurada.Text = "Id da Roupa Procurada";
+            // 
+            // txtiddaprocuradaform
+            // 
+            this.txtiddaprocuradaform.Location = new System.Drawing.Point(12, 480);
+            this.txtiddaprocuradaform.Name = "txtiddaprocuradaform";
+            this.txtiddaprocuradaform.Size = new System.Drawing.Size(175, 23);
+            this.txtiddaprocuradaform.TabIndex = 2;
+            // 
+            // btnalteraform
+            // 
+            this.btnalteraform.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnalteraform.Location = new System.Drawing.Point(371, 509);
+            this.btnalteraform.Name = "btnalteraform";
+            this.btnalteraform.Size = new System.Drawing.Size(104, 47);
+            this.btnalteraform.TabIndex = 1;
+            this.btnalteraform.Text = "ALTERAR";
+            this.btnalteraform.UseVisualStyleBackColor = true;
+            this.btnalteraform.Click += new System.EventHandler(this.btnalteraform_Click);
+            // 
+            // btndelroupaform
+            // 
+            this.btndelroupaform.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btndelroupaform.Location = new System.Drawing.Point(495, 509);
+            this.btndelroupaform.Name = "btndelroupaform";
+            this.btndelroupaform.Size = new System.Drawing.Size(104, 47);
+            this.btndelroupaform.TabIndex = 1;
+            this.btndelroupaform.Text = "REMOVER";
+            this.btndelroupaform.UseVisualStyleBackColor = true;
+            this.btndelroupaform.Click += new System.EventHandler(this.btndelroupaform_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(611, 566);
+            this.Controls.Add(this.btndelroupaform);
+            this.Controls.Add(this.btnalteraform);
+            this.Controls.Add(this.txtiddaprocuradaform);
+            this.Controls.Add(this.Iddaprocurada);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.txtmonth);
+            this.Controls.Add(this.txtday);
+            this.Controls.Add(this.colorsset);
+            this.Controls.Add(this.clothestype);
+            this.Controls.Add(this.typesoffabric);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtlistofclothesform);
+            this.Controls.Add(this.txtobsform);
+            this.Controls.Add(this.txtclothestyle);
+            this.Controls.Add(this.btnseekform);
+            this.Controls.Add(this.btnshowallform);
+            this.Controls.Add(this.btnaddform);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "App";
+            this.Text = "App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnaddform;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnshowallform;
+        private System.Windows.Forms.Button btnseekform;
+        private System.Windows.Forms.TextBox txtclothestyle;
+        private System.Windows.Forms.TextBox txtobsform;
+        private System.Windows.Forms.TextBox txtlistofclothesform;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox typesoffabric;
+        private System.Windows.Forms.ComboBox clothestype;
+        private System.Windows.Forms.ComboBox colorsset;
+        private System.Windows.Forms.TextBox txtday;
+        private System.Windows.Forms.TextBox txtmonth;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.Label Iddaprocurada;
+        private System.Windows.Forms.TextBox txtiddaprocuradaform;
+        private System.Windows.Forms.Button btnalteraform;
+        private System.Windows.Forms.Button btndelroupaform;
+    }
+}
---
+++ App.cs
@@ -0,0 +1,157 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Closetcontrol.Models;
+
+namespace WardrobApp
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void btnaddform_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch($"{txtmonth.Text}-{txtday.Text}-{txtyear.Text}", 
+                @"^(0[1-9]|1[012])[- -.](0[1-9]|[12][0-9]|3[01])[- -.](19|20)+\d\d$"))
+            {
+                var roupa = new Roupa()
+                {
+                    Id = Math.Abs(Guid.NewGuid().GetHashCode()),
+                    Tipo = clothestype.SelectedItem.ToString(),
+                    Estilo = txtclothestyle.Text,
+                    Tecido = typesoffabric.SelectedItem.ToString(),
+                    Cor = colorsset.SelectedItem.ToString(),
+                    Observacao = txtobsform.Text,
+                    DatadeUso = $"{txtyear.Text}-{txtmonth.Text}-{txtday.Text}",
+                };
+                var Url = "http://localhost:59603/WardrobeControl/postone";
+                var httpClient = new HttpClient();
+                var request = httpClient.PostAsync(Url,
+                    new StringContent(JsonConvert.SerializeObject(roupa),
+                    Encoding.UTF8, "application/json"));
+                request.Wait();
+
+                var result = request.Result.Content.ReadAsStringAsync();
+                result.Wait();
+                if(result == null)
+                    txtlistofclothesform.Text = "Desculpa bonitinha, mas você não tem essa roupa!";
+                List<Roupa> minhalista = new List<Roupa>();
+                minhalista = JsonConvert.DeserializeObject<List<Roupa>>(result.Result);
+                txtlistofclothesform.Text = "";
+                minhalista.ForEach(s => {
+                    txtlistofclothesform.Text += s.Id + " " + s.Tipo + " " + s.Estilo + " "
+                    + s.Cor + " " + s.Tecido + " " + s.Observacao + " " +
+                    s.DatadeUso + Environment.NewLine;
+                });
+            }
+            else
+              txtlistofclothesform.Text = "Corrija a data, escreva os campos na ordem:\n DIA MÊS e ANO";
+        }
+
+        private void btnshowallform_Click(object sender, EventArgs e)
+        {
+
+            var Url = "http://localhost:59603/WardrobeControl/getallofthem";
+            var httpClient = new HttpClient();
+            var resultRequest = httpClient.GetAsync(Url);
+            resultRequest.Wait();
+
+            var result = resultRequest.Result.Content.ReadAsStringAsync();
+            result.Wait();
+
+            List<Roupa> minhalista = new List<Roupa>();
+            minhalista = JsonConvert.DeserializeObject<List<Roupa>>(result.Result);
+            txtlistofclothesform.Text = "";
+            minhalista.ForEach(s => {
+                txtlistofclothesform.Text += s.Id + " " + s.Tipo + " " + s.Estilo + " "
+                + s.Cor + " " + s.Tecido + " " + s.Observacao + " " + DateTime.Parse(s.DatadeUso).ToString("dd/MM/yyyy")
+                 + Environment.NewLine;
+            });
+
+        }
+
+        private void btnseekform_Click(object sender, EventArgs e)
+        {
+            var Url = "http://localhost:59603/WardrobeControl/seekonebytype";
+            var httpClient = new HttpClient();
+            var resultRequest = httpClient.GetAsync($"{Url}?TipodaRoupa={clothestype.SelectedItem}");
+            resultRequest.Wait();
+
+            var result = resultRequest.Result.Content.ReadAsStringAsync();
+            result.Wait();
+
+            List<Roupa> minhalista = new List<Roupa>();
+            minhalista = JsonConvert.DeserializeObject<List<Roupa>>(result.Result);
+            txtlistofclothesform.Text = "";
+            if(minhalista.Count!=0)
+            {
+                minhalista.ForEach(s =>
+                {
+                    txtlistofclothesform.Text += s.Id + " " + s.Tipo + " " + s.Estilo + " "
+                    + s.Cor + " " + s.Tecido + " " + s.Observacao + " " +
+                    s.DatadeUso + Environment.NewLine;
+                });
+            }
+            else
+                txtlistofclothesform.Text += "Desculpe bonitinha, mas você não tem essa roupa!";
+        }
+
+        private void btnalteraform_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch($"{txtmonth.Text}-{txtday.Text}-{txtyear.Text}",
+                @"^(0[1-9]|1[012])[- -.](0[1-9]|[12][0-9]|3[01])[- -.](19|20)+\d\d$"))
+            {
+                var roupa = new Roupa()
+                {
+                    Id = Int32.Parse(txtiddaprocuradaform.Text),
+                    Tipo = null,
+                    Estilo = null,
+                    Tecido = null,
+                    Cor = null,
+                    Observacao = txtobsform.Text,
+                    DatadeUso = $"{txtyear.Text}-{txtmonth.Text}-{txtday.Text}",
+                };
+                var Url = "http://localhost:59603/WardrobeControl/changeinfoofone";
+                var httpClient = new HttpClient();
+                var request = httpClient.PostAsync(Url,
+                    new StringContent(JsonConvert.SerializeObject(roupa),
+                    Encoding.UTF8, "application/json"));
+                request.Wait();
+                var result = request.Result.Content.ReadAsStringAsync();
+                result.Wait();
+
+                txtlistofclothesform.Text = "";
+                if (result.AsyncState != null)
+                {
+                    txtlistofclothesform.Text = bool.Parse(result.Result) ? "A informação da roupa foi atualizada!" :
+                         "Desculpe bonitinha, mas você não tem essa roupa!";
+                }
+                else
+                    txtlistofclothesform.Text = "Desculpe bonitinha, mas você não tem essa roupa!";
+            }
+            else
+                txtlistofclothesform.Text = "Corrija a data, escreva os campos na ordem:\n DIA MÊS e ANO";
+
+        }
+
+        private void btndelroupaform_Click(object sender, EventArgs e)
+        {
+            var URL = "http://localhost:59603/WardrobeControl/deleteallwiththatid";
+            var httpClient = new HttpClient();
+            var resultRequestDelete = httpClient.DeleteAsync($"{URL}?IddaRoupa={txtiddaprocuradaform.Text}");
+            resultRequestDelete.Wait();
+
+            var resultDelete = resultRequestDelete.Result.Content.ReadAsStringAsync();
+            resultDelete.Wait();
+            txtlistofclothesform.Text = "Roupa removida do seu guarda-roupas!";
+        }
+    }
+}
